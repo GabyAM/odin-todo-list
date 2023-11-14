7aabb8a7b7f3f5c729319a21f3c705f9d3e0bcec
GabyAM <gdgabyam@gmail.com>
2023-11-14 11:11:30 -0300

    adds interactivity to buttons
    more specifically, to the category buttons and the add todo button.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import { displayModule } from "./display.js";
 import { todoController } from "./todoList.js";
 
-console.log('works...')
+const $addTodoButton = document.querySelector('.add-todo-button');
+$addTodoButton.addEventListener('click', () => {
+    displayModule.displayTodoPlaceholder();
+})
+
+const $categoryButtons = document.querySelectorAll('aside ul button');
+$categoryButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayModule.changeCategory(button.dataset.category);
+    })
+})
+/*
