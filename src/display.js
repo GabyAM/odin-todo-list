c1c58e9b9b4e70651e099b6c5474678a884a3822
GabyAM <gdgabyam@gmail.com>
2023-11-17 10:10:47 -0300

    adds functionality to the todo edit menu
    now, when pressing the edit button on a todo, the edit menu will open and will allow to change data from the todo, for the moment, only the title and completed state can be changed.
    This code need to be refactored.

--- src/display.js
+++ src/display.js
@@ -6,8 +6,8 @@ export const displayModule = (function() {
 
     function addEvents(todo) {
 
-        const todoTitle = todo.children[0];
-        const todoCompleted = todo.children[1];
+        const todoTitle = todo.children[1];
+        const todoCompleted = todo.children[0];
 
         todoTitle.removeEventListener('blur', onBlurHandler)
         todoTitle.removeEventListener('keydown', onKeyDownHandler)
@@ -41,7 +41,7 @@ export const displayModule = (function() {
 
         function onChange() {
             todoInterface.handleCompletedChange(todo.dataset.id);
-            //i dont need to update the list here!
+            //updateTodos();
         }
 
         function onBlurHandler() {
@@ -66,6 +66,7 @@ export const displayModule = (function() {
 
     function createTodo({title = '', completed = false, id = null} = {}) {
         const $listItem = document.createElement('li');
+        $listItem.className = 'todo';
         if(id) {
             $listItem.dataset.id = id;
         }
@@ -73,15 +74,40 @@ export const displayModule = (function() {
         const $todoTitle = document.createElement('input');
         $todoTitle.type = 'text';
         $todoTitle.value = title;
-        $todoTitle.className = 'todo-title';
+        $todoTitle.className = completed ? 'todo-title completed' : 'todo-title';
 
         const $todoCompleted = document.createElement('input');
         $todoCompleted.type = 'checkbox';
         $todoCompleted.checked = completed;
         $todoCompleted.className = 'todo-completed'
 
-        $listItem.appendChild($todoTitle);
+
+        const $todoOptions = document.createElement('buttton');
+        $todoOptions.removeEventListener('click', onClickHandler);
+
+        $todoOptions.className = 'todo-options';
+        const $optionsIcon = document.createElement('span');
+        $todoOptions.className = 'material-symbols-outlined';
+        $todoOptions.textContent = 'more_vert';
+        $todoOptions.appendChild($optionsIcon);
+
+        function onClick() {
+            if(document.querySelector('.todo-edit').style.width === '') {
+                showEditMenu();
+                updateEditMenu($listItem);
+            }
+            else {
+                updateEditMenu($listItem);
+            }
+        }
+        function onClickHandler() {
+            onClick();
+        }
+        $todoOptions.addEventListener('click', onClickHandler);
+
         $listItem.appendChild($todoCompleted);
+        $listItem.appendChild($todoTitle);
+        $listItem.appendChild($todoOptions);
 
         addEvents($listItem);
         return $listItem
@@ -98,13 +124,17 @@ export const displayModule = (function() {
     let updatingTodos = false;
 
     function updateTodos() {
-
+        console.log('updateTodos')
         if(!updatingTodos) {
             updatingTodos = true;
             document.querySelector('.todo-list').innerHTML = '';
             displayTodos(todoController.getTodos());
             updatingTodos = false;
         }
+    }
+
+    function updatePage() {
+        updateTodos();
     }
 
     function toggleAddTodoButton(type) {
@@ -127,12 +157,70 @@ export const displayModule = (function() {
         $todoTitle.focus();
     }
 
+    function showEditMenu() {
+        const $todoEditMenu = document.querySelector('.todo-edit');
+        $todoEditMenu.style.width = 'calc(100% - ((100% / 7) * 5))';
+    }   
+
+    function updateEditMenuFields(todo) {
+        document.querySelector('.edit-completed').checked = todo.querySelector('.todo.completed').checked;
+        document.querySelector('.edit-title').value = todo.querySelector('.todo-title').value;
+    }
+
+    const editMenuListeners = {
+        textInputListener: null,
+        checkboxListener: null
+    }
+    
+    function updateEditMenu(todo) {
+        const $todoEditMenu = document.querySelector('.todo-edit');
+        $todoEditMenu.dataset.id = todo.dataset.id;
+        
+        const $editCompleted = $todoEditMenu.querySelector('.todo-completed');
+        const $editTitle = $todoEditMenu.querySelector('.todo-title');
+        updateEditMenuFields(todo);
+
+        $editCompleted.removeEventListener('change', editMenuListeners.checkboxListener)
+        $editTitle.removeEventListener('keydown', editMenuListeners.textInputListener);
+
+        function onChange() {
+            todoInterface.handleCompletedChange(todo.dataset.id)
+            updatePage();
+        }
+
+        function onChangeHandler() {
+            onChange();
+        }
+
+        function onKeyDown(event) {
+            function getEditingTodo() {
+                const $domTodos = document.querySelectorAll('.todo');
+                return [...$domTodos].find(domTodo => domTodo.dataset.id === $todoEditMenu.dataset.id)
+            }
+            if(event.key === 'Enter') {
+                todoInterface.updateTodoTitle($todoEditMenu.dataset.id, $editTitle.value);
+                updateTodos();
+                updateEditMenuFields(getEditingTodo());
+            }
+        }
+
+        function onKeyDownHandler(e) {
+            onKeyDown(e);
+        }
+
+
+        $editCompleted.addEventListener('change', onChangeHandler)
+        editMenuListeners.checkboxListener = onChangeHandler;
+        $editTitle.addEventListener('keydown', onKeyDownHandler)
+        editMenuListeners.textInputListener = onKeyDownHandler;
+    }
+
     function changeCategory(categoryName) {
         todoController.switchCategory(categoryName);
         updateTodos();
     }
 
-    updateTodos();
+    updatePage();
 
     return {
         changeCategory, 
--- src/todoInterface.js
+++ src/todoInterface.js
@@ -4,27 +4,25 @@ import { todoController } from "./todoList.js";
 
 export const todoInterface = (function () {
 
+    function updateTodoTitle(id, todoTitle) {
+        const todoToEdit = todoController.getTodoById(id);
+        if(todoToEdit) {
+            todoToEdit.title = todoTitle;
+        } else {
+            throw new Error("can't find the todo by id on the list")
+        }
+    }
+
     function handleTodoSubmit(domTodo) {
-        const todoTitle = domTodo.children[0];
+        const todoTitle = domTodo.children[1];
 
         function addNewTodo() {
             const newTodo = new Todo(todoTitle.value, '', false, '', '');
             todoController.addTodo(newTodo);
         }
-    
-        function updateTodoTitle() {
-            const id = domTodo.dataset.id;
-    
-            const todoToEdit = todoController.getTodoById(id);
-            if(todoToEdit) {
-                todoToEdit.title = todoTitle.value;
-            } else {
-                throw new Error("can't find the todo by id on the list")
-            }
-        }
 
         if(domTodo.dataset.id) {
-            updateTodoTitle();
+            updateTodoTitle(domTodo.dataset.id, todoTitle.value);
         }
         else {
             domTodo.classList.remove('placeholder');
@@ -39,5 +37,5 @@ export const todoInterface = (function () {
         todoInList.toggleCompleted();
     }
 
-    return { handleTodoSubmit, handleCompletedChange }
+    return { handleTodoSubmit, updateTodoTitle, handleCompletedChange }
 })()
